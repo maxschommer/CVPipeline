b9c23fc9dc4ed5ef312a4cd86c4ec80010af43c7
Max Schommer <maxschommer@gmail.com>
2020-01-18 14:09:02 -0600

    Cleaned up code

--- pipeline.py
+++ pipeline.py
@@ -2,15 +2,16 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
         QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
-        QVBoxLayout, QWidget)
+        QVBoxLayout, QWidget, QFileDialog)
 import numpy as np
 import cv2
 
 
 class ProcessingFunc(object):
-    """An Image Processing function wrapper which contains the function and argument options. The
-    argument options are things such as type, range, display, default, etc. to describe how to make the
-    GUI for the function."""
+    """An Image Processing function wrapper which contains the function and 
+    argument options. The argument options are things such as type, range, 
+    display, default, etc. to describe how to make the GUI for the function.
+    """
     def __init__(self, func, args):
         super(ProcessingFunc, self).__init__()
         self.func = func
@@ -18,7 +19,8 @@ class ProcessingFunc(object):
         self.ret = FuncRet()
 
     def run_func(self):
-        """Runs the processing function, storing the return value as an array in the attribute 'ret'.
+        """Runs the processing function, storing the return value as an 
+        array in the attribute 'ret'.
         """
         args_list = []
         for arg in self.args:
@@ -27,8 +29,43 @@ class ProcessingFunc(object):
         for i, ret_val in enumerate(ret_arr):
             self.ret[i].val = ret_val
 
+class FileDialogueLine(object):
+    def __init__(self):
+        super(FileDialogueLine, self).__init__()
+        self.text_edit = QLineEdit() # contents.setText(data)
+        self.browse_btn = QPushButton("Browse")
+        self.browse_btn.clicked.connect(self.get_file) 
+        
+    
+    def get_components(self):
+        return [self.text_edit, self.browse_btn]
+
+    def get_file(self):
+        """Helper GUI function to retrieve a file.
+        
+        Returns:
+            str: A string representing the file path. 
+        """
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.AnyFile)
+        print("Getting File!")
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            self.text_edit.setText(filenames[0])
+
+    def __getitem__(self, key):
+        return self.get_components()[key]
+
+    # def __iter__(self):
+    #     return self.get_components().__iter__()
+
+    # def __del__(self):
+    #     print('Deleting File Dialogue: ', id(self))
+
+ 
 class FuncArgs(object):
-    """An Argument object to describe the type of parameters, defaults, and GUI options.
+    """An Argument object to describe the type of parameters, defaults, 
+    and GUI options.
 
     Args:
         arg_type (str): String describing the type of the argument
@@ -41,8 +78,8 @@ class FuncArgs(object):
             -- table_widget
             -- line_edit
             -- slider
-        gui_range (arr): A two-tuple describing the minimum and maximum values for
-                a numeric parameter. 
+        gui_range (arr): A two-tuple describing the minimum and maximum 
+                values for a numeric parameter. 
         default (any): The default value of the parameter. 
     """
     def __init__(self, **kwargs):
@@ -95,12 +132,16 @@ class FuncArgs(object):
             if self.default is not None:
                 self.value = self.default
             else: # If no value or default value
-                if self.gui_range is not None: # If the parameter Gui Range exists
-                    self.value = np.mean(self.gui_range) # Set the value to halfway down the range.
+                if self.gui_range is not None:  # If the parameter Gui Range 
+                                                # exists
+                    self.value = np.mean(self.gui_range) # Set the value 
+                                                         # to halfway down 
+                                                         # the range.
 
 
-        # This is to check if it is an argument which was the return value of a previous
-        # stage in the pipeline. If this is true, the value should be extracted.
+        # This is to check if it is an argument which was the return value 
+        # of a previous stage in the pipeline. If this is true, the value 
+        # should be extracted.
         if isinstance(self.value, RetVal):
             self.value = self.value.get_val()
 
@@ -108,7 +149,23 @@ class FuncArgs(object):
 
 
 
-    def get_gui_component(self, component_type="default"):
+    def get_gui_components(self, component_type="default"):
+        """Retrieves the GUI components needed for the argument to be edited.
+        
+        Args:
+            component_type (str, optional): The type of component to use, 
+                    if there are multiple options availiable. For example, 
+                    a list of options can be selected by a series of radio 
+                    buttons or by a dropdown menu, so forcing one over the 
+                    other is done with component_type. 
+        
+        Returns:
+            arr[QTWidget]: An array of QT widgets to be added to an 
+                    existing layout.
+        
+        Raises:
+            ValueError: Description
+        """
         res = None
         # Logic to figure out which GUI component to use. 
         if self.gui_type is not None:
@@ -119,16 +176,16 @@ class FuncArgs(object):
                 if self.gui_array is not None:
                     res.addItems(self.gui_array)
                     res.setCurrentIndex(self.gui_array.index(self.get_val()))
-                    return res
+                    return [res]
                 else:
-                    raise ValueError("gui_array must be specified for combo_box.")
+                    raise ValueError("gui_array must be specified \
+                        for combo_box.")
+            if self.gui_type == "file":
+                fDialog = FileDialogueLine()
+                return fDialog
 
             if self.gui_type == "check_box":
                 pass
-
-                
-
-
 
         else:
             pass
@@ -175,49 +232,54 @@ class FuncRet(object):
             return self.ret_vals[key]
         else:
             while key >= len(self.ret_vals):
-                self.ret_vals.append(RetVal()) # Add a RetVal object with the attribute val initialized as None, to be written later
+                self.ret_vals.append(RetVal())  # Add a RetVal object with 
+                                                # the attribute val 
+                                                # initialized as None, to 
+                                                # be written later
             return self.ret_vals[key]
 
     def __setitem__(self, key, value):
         self.ret_vals[key].val = value
 
 class ProcessingPipeline(object):
-    """Contains an array of ProcessingFunc and an input image, as well as arguments """
+    """Contains an array of ProcessingFunc and an input image, as well 
+    as arguments.
+    """
     def __init__(self, proc_funcs):
         super(ProcessingPipeline, self).__init__()
         self.proc_funcs = proc_funcs
 
     def run_pipeline(self):
         self.update_funcs()
-            # print(proc_func.ret)
-            # print(proc_func.args[0])
-        #     cv2.imshow("Image {}".format(i), proc_func.ret[0].val)
-        # cv2.waitKey(0)
-
 
         app = QApplication([])
         window = QWidget()
-        layout = QVBoxLayout()
-        main_tabs_widget = QTabWidget() # These are the tabs for the Image display and GUI inputs. 
+        layout = QGridLayout()
+        main_tabs_widget = QTabWidget() # These are the tabs for the Image 
+                                        # display and GUI inputs. 
+
 
         layout.addWidget(main_tabs_widget)
         tab_contents = []
+        tab_h_boxs = []
+
+        stored_gui_comps = []   # This is to prevent iterable objects with 
+                                # multiple gui components from getting deleted
         for func in self.proc_funcs:
             tab_contents.append(QWidget())
+
+            tab_h_boxs.append(QGridLayout())
+            tab_h_boxs[-1].setContentsMargins(5, 5, 5, 5)
+            for i, arg in enumerate(func.args):
+                gui_comps = arg.get_gui_components()
+                stored_gui_comps.append(gui_comps)
+                if gui_comps is not None:
+                    for j, gui_comp in enumerate(gui_comps):
+                        tab_h_boxs[-1].addWidget(gui_comp, i, j)
+
+            tab_contents[-1].setLayout(tab_h_boxs[-1])
             main_tabs_widget.addTab(tab_contents[-1], func.func.__name__)
-            
-            for arg in func.args:
-                gui_comp = arg.get_gui_component()
-                if gui_comp is not None:
-                    layout.addWidget(gui_comp)
-
-
-        # layout.addWidget(QPushButton('Top'))
-        # layout.addWidget(QPushButton('Bottom'))
-        # layout.addWidget(QLabel("Test"))
-        # res = QComboBox()
-        # res.addItems(["A", "b", "3"])
-        # layout.addWidget(res)
+        
         window.setLayout(layout)
         window.show()
         app.exec_()
@@ -256,6 +318,8 @@ def main():
     file = '/home/max/Videos/VID_20190403_105950~2_no_audio(1).mp4'
     f1_args = [FuncArgs(arg_type="str", gui_input=True, default=file, gui_type="file"), 
                 FuncArgs(arg_type="int", gui_input=True, gui_type="combo_box", gui_array=["One", "Two", "Three"], default="Two")]
+
+    # f1_rets = [] TODO: Make return values labeled in order to display them.
     f1 = ProcessingFunc(load_img, f1_args)
 
 
